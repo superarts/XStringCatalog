9909f778b27b483f66fef990883d0a9de9147a77
jinkim2739 <67286328+jinkim2739@users.noreply.github.com>
2023-12-18 14:04:41 -0800

    Refactor: Create StringKeyModel and separated functions from the StringCatalogEnum struct

--- StringCatalogEnum/Sources/StringCatalogEnum/main.swift
+++ StringCatalogEnum/Sources/StringCatalogEnum/main.swift
@@ -29,6 +29,8 @@ struct StringCatalogEnum: ParsableCommand {
     var enumTypealias: String = "XCS"
 
     func run() throws {
+        let model = StringKeyModel()
+        print("Model: ", model.self)
         print("LOADING: \(xcstringsPath)")
         let url = URL(fileURLWithPath: xcstringsPath)
         let data = try Data(contentsOf: url)
@@ -56,125 +58,38 @@ struct StringCatalogEnum: ParsableCommand {
             enum \(enumName): String, CaseIterable {
 
             """
-
-        var cases = [String]()
-        var knownCases = [String]()
-        for (key, _) in strings {
-            guard let name = convertToVariableName(key: key) else {
-                print("SKIPPING: \(key)")
-                continue
-            }
-            guard key == name else {
-                continue
-            }
-            guard !knownCases.contains(name) else {
-                cases.append("    // TODO: fix duplicated entry - case \(name)\n")
-                continue
-            }
-            knownCases.append(name)
-
-            // print("\(name):\t\(key)")
-            // TODO: extract `localizations.en.stringUnit.value` and add in comments as inline documents
-            if Keyword.allCases.map({ $0.rawValue }).contains(name) {
-                cases.append("    case `\(name)`\n")
-            } else {
-                cases.append("    case \(name)\n")
-            }
-        }
-        cases.sort()
-        cases.forEach { string in
-            output += string
-        }
+        let keywordRawValues = getKeywordRawValues()
+        let firstCases = model.createEnumKeys(with: strings, keyNameMatches: true, keywordEnum: keywordRawValues)
+        let secondCases = model.createEnumKeys(with: strings, keyNameMatches: false, keywordEnum: keywordRawValues)
 
         output += """
+                    
+        \(firstCases)
+                    
+        // MARK: - The following cases should be manually replaced in your codebase.
+                    
+        \(secondCases)
 
-                // MARK: - The following cases should be manually replaced in your codebase.
+        /// Usage: `SwiftUI.Text(\(enumTypealias).yourStringCatalogKey.key)`
+        var key: LocalizedStringKey { LocalizedStringKey(rawValue) }
 
+        var string: String { NSLocalizedString(self.rawValue, comment: "Generated localization from String Catalog key: \\(key)") }
 
-            """
-        cases.removeAll()
-        for (key, _) in strings {
-            guard let name = convertToVariableName(key: key) else {
-                print("SKIPPING: \(key)")
-                continue
-            }
-            guard key != name else {
-                continue
-            }
-            guard !knownCases.contains(name) else {
-                cases.append("    // TODO: fix duplicated entry - case \(name)\n")
-                continue
-            }
-            knownCases.append(name)
-
-            // print("\(name):\t\(key)")
-            // TODO: probably missing " handling?
-            if Keyword.allCases.map({ $0.rawValue }).contains(name) {
-                cases.append("    case `\(name)` = \"\(key.replacingOccurrences(of: "\n", with: ""))\"\n")
-            } else {
-                cases.append("    case \(name) = \"\(key.replacingOccurrences(of: "\n", with: ""))\"\n")
-            }
+        // var text: String.LocalizationValue { String.LocalizationValue(rawValue) }
         }
-        // cases = Array(Set<String>(cases))
-        cases.sort()
-        cases.forEach { string in
-            output += string
-        }
+        // swiftlint:enable all
+        """
 
-        output += """
-
-                /// Usage: `SwiftUI.Text(\(enumTypealias).yourStringCatalogKey.key)`
-                var key: LocalizedStringKey { LocalizedStringKey(rawValue) }
-
-                var string: String { NSLocalizedString(self.rawValue, comment: "Generated localization from String Catalog key: \\(key)") }
-
-                // var text: String.LocalizationValue { String.LocalizationValue(rawValue) }
-            }
-            // swiftlint:enable all
-            """
         print(output)
         let outputURL = URL(fileURLWithPath: outputFilename)
         try output.write(to: outputURL, atomically: true, encoding: .utf8)
         print("Written to: \(outputFilename)")
     }
-
-    /// Convert a Strint Catalog key to a Swift variable name.
-    private func convertToVariableName(key: String) -> String? {
-        // Leave only letters and numeric characters
-        var result = key.components(separatedBy: CharacterSet.letters.union(CharacterSet.alphanumerics).inverted).joined()
-
-        // Remove leading numeric characters
-        while !result.isEmpty {
-            let firstLetter = result.prefix(1)
-            let digitsCharacters = CharacterSet(charactersIn: "0123456789")
-            if CharacterSet(charactersIn: String(firstLetter)).isSubset(of: digitsCharacters) {
-                // print("dropping first of: \(result)")
-                result = String(result.dropFirst())
-            } else {
-                break
-            }
-        }
-
-        // Return nil if empty
-        guard !result.isEmpty else {
-            return nil
-        }
-
-        // Return lowercased string if there's only 1 character
-        guard result.count > 1 else {
-            return result.lowercased()
-        }
-
-        // Change the first character to lowercase
-        let firstLetter = result.prefix(1).lowercased()
-        let remainingLetters = result.dropFirst()
-        result = firstLetter + remainingLetters
-
-        // TODO: uppercase remaining words, e.g. "an example" to "anExample"; currently it's "anexample"
-        // TODO: lowercase capitalized words, e.g. "EXAMPLE" to "example"; currently it's "eXAMPLE"
-
-        return result
+    
+    func getKeywordRawValues() -> [String] {
+        return Keyword.allCases.map { $0.rawValue }
     }
+
 }
 
 StringCatalogEnum.main()
--- StringCatalogEnum/Sources/StringCatalogEnum/model.swift
+++ StringCatalogEnum/Sources/StringCatalogEnum/model.swift
@@ -7,3 +7,103 @@ struct XCStrings: Decodable {
     let strings: [String: String]
 }
 */
+
+import Foundation
+
+struct StringKeyModel {
+    
+    /// Creates enum cases depending on whether key == name
+    ///   - Parameters:
+    ///     - stringData: A dictionary containing string data.
+    ///     - keyNameMatches: A boolean flag indicating whether the enum cases should match the keys exactly.
+    ///     - keywordEnum: An array of raw values from the Keyword enum in StringCatalogEnum struct
+    func createEnumKeys(with stringData: [String: Any], keyNameMatches: Bool, keywordEnum: [String]) -> String {
+        var partialOutput = ""
+        var cases = [String]()
+        var knownCases = [String]()
+        for (key, _) in stringData {
+            
+            guard let name = convertToVariableName(key: key) else {
+                print("SKIPPING: \(key)")
+                continue
+            }
+            
+            if keyNameMatches {
+                guard key == name else {
+                    continue
+                }
+            } else {
+                guard key != name else {
+                    continue
+                }
+            }
+            
+            guard !knownCases.contains(name) else {
+                cases.append("    // TODO: fix duplicated entry - case \(name)\n")
+                continue
+            }
+            knownCases.append(name)
+
+            // TODO: extract `localizations.en.stringUnit.value` and add in comments as inline documents
+
+            let caseString: String
+            if keywordEnum.contains(name) {
+                caseString = keyNameMatches
+                    ? "    case `\(name)`\n"
+                    : "    case `\(name)` = \"\(key.replacingOccurrences(of: "\n", with: ""))\"\n"
+            } else {
+                caseString = keyNameMatches
+                    ? "    case \(name)\n"
+                    : "    case \(name) = \"\(key.replacingOccurrences(of: "\n", with: ""))\"\n"
+            }
+
+            cases.append(caseString)
+        }
+        cases.sort()
+        cases.forEach { string in
+            partialOutput += string
+        }
+
+        return partialOutput
+    }
+
+   
+    
+    /// Convert a Strint Catalog key to a Swift variable name.
+    func convertToVariableName(key: String) -> String? {
+        // Leave only letters and numeric characters
+        var result = key.components(separatedBy: CharacterSet.letters.union(CharacterSet.alphanumerics).inverted).joined()
+
+        // Remove leading numeric characters
+        while !result.isEmpty {
+            let firstLetter = result.prefix(1)
+            let digitsCharacters = CharacterSet(charactersIn: "0123456789")
+            if CharacterSet(charactersIn: String(firstLetter)).isSubset(of: digitsCharacters) {
+                // print("dropping first of: \(result)")
+                result = String(result.dropFirst())
+            } else {
+                break
+            }
+        }
+
+        // Return nil if empty
+        guard !result.isEmpty else {
+            return nil
+        }
+
+        // Return lowercased string if there's only 1 character
+        guard result.count > 1 else {
+            return result.lowercased()
+        }
+
+        // Change the first character to lowercase
+        let firstLetter = result.prefix(1).lowercased()
+        let remainingLetters = result.dropFirst()
+        result = firstLetter + remainingLetters
+
+        // TODO: uppercase remaining words, e.g. "an example" to "anExample"; currently it's "anexample"
+        // TODO: lowercase capitalized words, e.g. "EXAMPLE" to "example"; currently it's "eXAMPLE"
+
+        return result
+    }
+}
