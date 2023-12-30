f47ceb86b8155cfc2d8d618c8a773adbe41bee15
Uni-boy <liu.xinzh@northeastern.edu>
2023-12-30 04:00:22 -0500

    Model Finalized

--- StringCatalogEnum/Sources/StringCatalogEnum/model.swift
+++ StringCatalogEnum/Sources/StringCatalogEnum/model.swift
@@ -1,22 +1,73 @@
-// TODO: make XCStrings Decodable by listing all possible models
-// Example: let obj = try JSONDecoder().decode(XCStrings.self, from: data)
-/*
+/// Represents the root structure of the xcstrings JSON.
+/// This struct is designed to handle various JSON formats, including those
+/// generated from SwiftUI and manually added translations.
+/// 
+/// Examples of supported JSON formats:
+///
+/// 1. Generated from SwiftUI (no translation added):
+/// ```
+/// "Home": {}
+/// ```
+///
+/// 2. Generated from SwiftUI, with an English translation:
+/// ```
+/// "Login": {
+///   "localizations": {
+///     "en": {
+///       "stringUnit": {
+///         "state": "translated",
+///         "value": "Login"
+///       }
+///     }
+///   }
+/// }
+/// ```
+///
+/// 3. Manually added, English only:
+/// ```
+/// "welcomeBack": {
+///   "extractionState": "manual",
+///   "localizations": {
+///     "en": {
+///       "stringUnit": {
+///         "state": "translated",
+///         "value": "Welcome back"
+///       }
+///     }
+///   }
+/// }
+/// ```
+
 struct XCStrings: Decodable {
     let sourceLanguage: String
     let version: String
-    let strings: [String: String]
-}
-*/
-struct XCStrings: Decodable {
     let strings: [String: StringInfo]
 }
 
 struct StringInfo: Decodable {
     let extractionState: String?
-    let localizations: [String: Localization]
+    let localizations: [String: Localization]?
 }
 
 struct Localization: Decodable {
+    let variations: Variations?
+}
+
+struct Variations: Decodable {
+    let plural: PluralVariations?
+    let device: DeviceVariations?
+}
+
+struct PluralVariations: Decodable {
+    let one: StringUnitWrapper?
+    let other: StringUnitWrapper?
+}
+
+struct DeviceVariations: Decodable {
+    let variations: [String: StringUnitWrapper]?
+}
+
+struct StringUnitWrapper: Decodable {
     let stringUnit: StringUnit
 }
 
--- StringCatalogEnum/Tests/StringCatalogEnumTests/StringCatalogEnumTests.swift
+++ StringCatalogEnum/Tests/StringCatalogEnumTests/StringCatalogEnumTests.swift
@@ -5,10 +5,29 @@ import Quick
 final class StringCatalogEnumSpec: QuickSpec {
     override class func spec() {
         context("StringCatalogEnum") {
-            describe("Example") {
-                it("should be replaced") {
-                    expect(true).toNot(equal(false))
+            describe("XCStrings Decoding") {
+                it("should decode simple key correctly") {
+                    let jsonString = """
+                    {
+                      "strings": {
+                        "Home": {}
+                      }
+                    }
+                    """
+                    let jsonData = Data(jsonString.utf8)
+                    let decoder = JSONDecoder()
+
+                    do {
+                        let xcStrings = try decoder.decode(XCStrings.self, from: jsonData)
+                        expect(xcStrings.strings["Home"]).toNot(beNil())
+                        expect(xcStrings.strings["Home"]?.extractionState).to(beNil())
+                        expect(xcStrings.strings["Home"]?.localizations.isEmpty).to(beTrue())
+                    } catch {
+                        fail("Decoding failed: \(error)")
+                    }
                 }
+
+                // Add other test cases here in similar fashion
             }
         }
     }
